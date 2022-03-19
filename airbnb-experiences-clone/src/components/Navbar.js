0bcbe6f53c0461ac0082eae6eee8a96b6b0a0840
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-03-19 15:58:37 +0100

    navbar modified

--- airbnb-experiences-clone/src/components/Navbar.js
+++ airbnb-experiences-clone/src/components/Navbar.js
@@ -1,11 +1,10 @@
-import React from "react"
 import logo from "../images/logo-airbnb.png"
 import "../style.css"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src={logo} alt="logo" width="109px" height="60px" className="nav--logo"/>
+            <img src={logo} alt="logo" width="109px" height="60px" className="nav--logo" />
         </nav>
     )
 }
