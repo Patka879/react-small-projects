070d8ee5bd4bfd690f32e979775bc83bcdd83427
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-03-19 19:01:10 +0100

    page layout created

---
+++ generator/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../style.css"
+import TrollFace from "../Images/Troll Face.svg"
+
+export default function Header () {
+    return(
+        <div className="header">
+            <span className="left-header">
+            <img className="left-header-img" src={TrollFace} alt="troll face" />
+            Meme Generator</span>
+            <span className="right-header">React Course - Project 3</span>
+        </div>
+    )
+}
