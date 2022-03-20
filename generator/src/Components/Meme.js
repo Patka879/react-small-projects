40c3de4379c91385245d08cbafbb254ca12a9b9c
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-03-20 21:31:53 +0100

    mappig of meme Data created

--- generator/src/Components/Meme.js
+++ generator/src/Components/Meme.js
@@ -1,10 +1,21 @@
-
+import memesData from "../memesData"
+import {useState} from "react"
 
 
 export default function Meme() {
+
+
+    const [memeImage, setMemeImage] = useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url) 
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -13,8 +24,12 @@ export default function Meme() {
                     type="text"
                     placeholder="Bottom text"
                     className="form--input" />
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
+                <button 
+                onClick={getMemeImage}
+                className="form--button">Get a new meme image 🖼
+                </button>
+            </div>
+            <img src={memeImage} className="meme-image" />
         </main>
     )
 }
