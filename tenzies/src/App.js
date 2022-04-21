6067d9c75d2d8046fa8e3802314dd2eb4c77440c
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-04-21 16:12:17 +0200

    styles created

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from './Components/Main'
+import React from 'react'
 
-function App() {
+export default function App() {
+
+  function allNewDice() {
+    const newNumbers = []
+
+    for(let i = 0; i < 10; i++)
+    newNumbers.push(Math.floor(Math.random() * 6) + 1)
+    
+    return newNumbers
+  }
+
+  console.log(allNewDice())
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Main />
     </div>
   );
 }
 
-export default App;
+
